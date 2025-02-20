36d81cfbe955a5274f1ec7ad7143c72c2e56edb4
Sanchez Felix <felixquintero847@gmail.com>
2025-02-19 22:52:53 -0700

    agregacion de dominio al usuario

---
+++ src/main/java/edu/student/itson/bda/ticketwizard/entidades/Usuario.java
@@ -0,0 +1,122 @@
+package edu.student.itson.bda.ticketwizard.entidades;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Objects;
+
+public class Usuario {
+
+    private final int id;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String email;
+    private int saldo;
+    private LocalDate fechaNacimiento;
+
+    public Usuario(int id, String nombre, String apellido, String direccion, String email, int saldo, LocalDate fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.email = email;
+        this.saldo = saldo;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+
+    public void incrementarSaldo(int monto) {
+        if (monto <= 0) {
+            throw new IllegalArgumentException("El monto debe ser positivo");
+        }
+        this.saldo += monto;
+    }
+
+    public void decrementarSaldo(int monto) {
+        if (monto <= 0) {
+            throw new IllegalArgumentException("El monto debe ser positivo");
+        }
+        if (monto > saldo) {
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+        this.saldo -= monto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", email=" + email + ", saldo=" + saldo + ", fechaNacimiento=" + fechaNacimiento + '}';
+    }
+}
