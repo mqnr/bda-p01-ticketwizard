0bd6615e4c45e4e74a2c5e2dd4c31d0464424359
mzamorano <martin@mzamorano.com>
2025-02-20 08:39:08 -0700

    agregar DAO de usuarios

---
+++ src/main/java/edu/student/itson/bda/ticketwizard/persistencia/UsuariosDAO.java
@@ -0,0 +1,132 @@
+package edu.student.itson.bda.ticketwizard.persistencia;
+
+import edu.student.itson.bda.ticketwizard.dtos.AgregarSaldoDTO;
+import edu.student.itson.bda.ticketwizard.dtos.BuscarUsuarioDTO;
+import edu.student.itson.bda.ticketwizard.entidades.Usuario;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsuariosDAO {
+
+    public void agregarSaldo(AgregarSaldoDTO agregarSaldoDTO) {
+        String codigoSQL = "UPDATE usuarios SET saldo = saldo + ? WHERE email = ?";
+
+        try {
+            Connection conexion = ManejadorConexiones.crearConexion();
+
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+
+            comando.setBigDecimal(1, agregarSaldoDTO.getMonto());
+            comando.setString(2, agregarSaldoDTO.getEmail());
+
+            int filasAfectadas = comando.executeUpdate();
+            if (filasAfectadas == 0) {
+                throw new RuntimeException("Usuario no encontrado: " + agregarSaldoDTO.getEmail());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al actualizar saldo", e);
+        }
+    }
+
+    public Usuario consultarUsuario(BuscarUsuarioDTO buscarUsuarioDTO) {
+        String codigoSQL = """
+                           SELECT
+                             id,
+                             email,
+                             nombre,
+                             apellido,
+                             direccion,
+                             fecha_nacimiento,
+                             saldo
+                           FROM usuarios WHERE email = ?
+                           """;
+
+        try {
+            Connection conexion = ManejadorConexiones.crearConexion();
+
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+
+            comando.setString(1, buscarUsuarioDTO.getEmail());
+
+            ResultSet resultadosConsulta = comando.executeQuery();
+            if (resultadosConsulta.next()) {
+                return new Usuario(
+                        resultadosConsulta.getInt("id"),
+                        resultadosConsulta.getString("nombre"),
+                        resultadosConsulta.getString("apellido"),
+                        resultadosConsulta.getString("direccion"),
+                        resultadosConsulta.getString("email"),
+                        resultadosConsulta.getBigDecimal("saldo"),
+                        resultadosConsulta.getDate("fecha_nacimiento").toLocalDate()
+                );
+            } else {
+                throw new RuntimeException("Usuario no encontrado: " + buscarUsuarioDTO.getEmail());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al consultar saldo", e);
+        }
+    }
+
+    public BigDecimal consultarSaldo(BuscarUsuarioDTO buscarUsuarioDTO) {
+        String codigoSQL = "SELECT saldo FROM usuarios WHERE email = ?";
+
+        try {
+            Connection conexion = ManejadorConexiones.crearConexion();
+
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+
+            comando.setString(1, buscarUsuarioDTO.getEmail());
+
+            ResultSet resultadosConsulta = comando.executeQuery();
+            if (resultadosConsulta.next()) {
+                return resultadosConsulta.getBigDecimal("saldo");
+            } else {
+                throw new RuntimeException("Usuario no encontrado: " + buscarUsuarioDTO.getEmail());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al consultar saldo", e);
+        }
+    }
+
+    public Usuario consultarUsuarioAleatorio() {
+        String codigoSQL = """
+                           SELECT
+                             id,
+                             email,
+                             nombre,
+                             apellido,
+                             direccion,
+                             fecha_nacimiento,
+                             saldo
+                           FROM usuarios
+                           ORDER BY RAND() LIMIT 1
+                           """;
+
+        try {
+            Connection conexion = ManejadorConexiones.crearConexion();
+
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+
+            ResultSet resultadosConsulta = comando.executeQuery();
+            if (resultadosConsulta.next()) {
+                return new Usuario(
+                        resultadosConsulta.getInt("id"),
+                        resultadosConsulta.getString("nombre"),
+                        resultadosConsulta.getString("apellido"),
+                        resultadosConsulta.getString("direccion"),
+                        resultadosConsulta.getString("email"),
+                        resultadosConsulta.getBigDecimal("saldo"),
+                        resultadosConsulta.getDate("fecha_nacimiento").toLocalDate()
+                );
+            } else {
+                throw new RuntimeException("No hay usuarios en la base de datos");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al consultar usuario aleatorio", e);
+        }
+    }
+}
