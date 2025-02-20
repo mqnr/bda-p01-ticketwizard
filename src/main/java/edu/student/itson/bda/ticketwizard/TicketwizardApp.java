4e58cf7f531132c410d20c870039cbd000d069e4
mzamorano <martin@mzamorano.com>
2025-02-20 09:41:20 -0700

    inicializar todo en la main class

--- src/main/java/edu/student/itson/bda/ticketwizard/TicketwizardApp.java
+++ src/main/java/edu/student/itson/bda/ticketwizard/TicketwizardApp.java
@@ -1,7 +1,13 @@
 package edu.student.itson.bda.ticketwizard;
 
+import edu.student.itson.bda.ticketwizard.control.ControlAgregarSaldo;
+import edu.student.itson.bda.ticketwizard.persistencia.UsuariosDAO;
+
 public class TicketwizardApp {
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo, de Ticketwizard");
+        UsuariosDAO usuariosDAO = new UsuariosDAO();
+        ControlAgregarSaldo control = new ControlAgregarSaldo(usuariosDAO);
+        control.iniciarCasoUso();
     }
 }
