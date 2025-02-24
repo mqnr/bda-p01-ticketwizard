5a95b5443500f377682a71a3d61458500d7c6c73
Bryan Alberto VE <brayanesquer13@gmail.com>
2025-02-23 18:12:47 -0700

    se resolvio el error visualizar los eventos

--- src/main/java/edu/student/itson/bda/ticketwizard/control/ControlEventos.java
+++ src/main/java/edu/student/itson/bda/ticketwizard/control/ControlEventos.java
@@ -3,6 +3,7 @@ package edu.student.itson.bda.ticketwizard.control;
 import edu.student.itson.bda.ticketwizard.entidades.Evento;
 import edu.student.itson.bda.ticketwizard.persistencia.EventosDAO;
 import edu.student.itson.bda.ticketwizard.presentacion.EventosDisponibles;
+import edu.student.itson.bda.ticketwizard.presentacion.InicioPerfil;
 import java.util.List;
 
 /**
@@ -11,11 +12,15 @@ import java.util.List;
  */
 public class ControlEventos {
 
-    private final EventosDAO eventosDAO;
+    private EventosDAO eventosDAO;
     private EventosDisponibles formCatalogoEventos;
+    private InicioPerfil formInicioPerfil;
+    private ControlUsuarios usuarios;
+    private ControlEventos controlEventos;
 
-    public ControlEventos(EventosDAO eventosDAO) {
+    public ControlEventos(EventosDAO eventosDAO, ControlUsuarios usuarios) {
         this.eventosDAO = eventosDAO;
+        this.usuarios = usuarios;
     }
 
     public void iniciarCasoUso() {
@@ -29,8 +34,15 @@ public class ControlEventos {
     }
 
     public void mostrarFormularioEventosDisponibles() {
-        this.formCatalogoEventos = new EventosDisponibles(this);
+        this.formInicioPerfil.dispose();
+        this.formCatalogoEventos = new EventosDisponibles(this.controlEventos);
         this.formCatalogoEventos.setVisible(true);
+    }
+
+    public void mostrarFormularioInicioPerfil() {
+        this.formCatalogoEventos.dispose();
+        this.formInicioPerfil = new InicioPerfil(this.usuarios);
+        this.formInicioPerfil.setVisible(true);
     }
 
     public List<Evento> consultarListaEventos() {
