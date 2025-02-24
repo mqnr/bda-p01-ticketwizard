59d052d2bbd97c4e066079e102ad1e30608c2177
Bryan Alberto VE <brayanesquer13@gmail.com>
2025-02-23 18:21:46 -0700

    agregue mas gets para manejo de informacion

--- src/main/java/edu/student/itson/bda/ticketwizard/dtos/ObtenerUsuarioDTO.java
+++ src/main/java/edu/student/itson/bda/ticketwizard/dtos/ObtenerUsuarioDTO.java
@@ -3,12 +3,34 @@ package edu.student.itson.bda.ticketwizard.dtos;
 public class ObtenerUsuarioDTO {
 
     private final int idUsuario;
+    private String nombre, apellido, direccion, email;
 
-    public ObtenerUsuarioDTO(int idUsuario) {
+    public ObtenerUsuarioDTO(int idUsuario, String nombre, String apellido, String direccion, String email) {
         this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.email = email;
     }
 
     public int getIdUsuario() {
         return idUsuario;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
