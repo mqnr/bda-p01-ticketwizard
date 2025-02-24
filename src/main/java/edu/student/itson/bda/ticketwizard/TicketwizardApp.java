0a5c27c6a8125e1d17e41d641913295576b30d37
mzamorano <martin@mzamorano.com>
2025-02-23 18:02:59 -0700

    agregar mecanismo de selección de GUI

--- src/main/java/edu/student/itson/bda/ticketwizard/TicketwizardApp.java
+++ src/main/java/edu/student/itson/bda/ticketwizard/TicketwizardApp.java
@@ -2,12 +2,31 @@ package edu.student.itson.bda.ticketwizard;
 
 import edu.student.itson.bda.ticketwizard.control.ControlUsuarios;
 import edu.student.itson.bda.ticketwizard.persistencia.UsuariosDAO;
+import edu.student.itson.bda.ticketwizard.presentacion.LoginFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class TicketwizardApp {
 
     public static void main(String[] args) {
-        UsuariosDAO usuariosDAO = new UsuariosDAO();
-        ControlUsuarios control = new ControlUsuarios(usuariosDAO);
-        control.iniciarCasoUso();
+        // la GUI alternativa
+        if (new java.io.File(".gui_alternativa").exists()) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+                JOptionPane.showMessageDialog(null,
+                        "No se pudo cargar el tema del sistema. Se usará el tema por defecto.",
+                        "Advertencia",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+            SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
+        } else {
+            // la GUI principal
+            UsuariosDAO usuariosDAO = new UsuariosDAO();
+            ControlUsuarios control = new ControlUsuarios(usuariosDAO);
+            control.iniciarCasoUso();
+        }
     }
 }
